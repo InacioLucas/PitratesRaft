f5351b0fd54364b5dd8e1ec9ed2dfa33469c2cc3
InacioLucas <lucas.inaciosilva@gmail.com>
2020-09-03 00:55:37 -0300

    primeiro commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public int damage = 1;
+    public float speed;
+
+    public GameObject effect;
+    
+   
+
+ 
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //efeito mexer a camera
+            
+
+            // efeito  particulas
+            Instantiate(effect, transform.position, Quaternion.identity);
+            
+            //player takes damage!
+            other.GetComponent<Player>().health -= damage;
+            
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public float health = 3;
+
+    public GameObject effect;
+
+    
+
+
+    private void Update()
+    {
+        if (health <= 0) {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight){
+
+            //efeito mexer a camera
+           
+
+            //efeitos de particula
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            //posicao do personagem 
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight){
+            //efeito mexer a camera
+            
+
+            //efeitos de particula
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            //posicao do personagem 
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+
+
+    }
+}
